d8ab42de04e39a81285a51a7ffdc7c7209859ef9
Dan <example@gmail.com>
2020-08-05 21:46:55 +0300

    this is the first commit

---
+++ Урок 1/script.js
@@ -0,0 +1,31 @@
+"use strict"; //декаларация о том, что используется стандарт ES6
+// var leftBorderWidth = 1;
+// let second = 2;
+// console.log(second);
+// const pi = 3.14;
+
+let person = {
+    name: "John",
+    age: 25,
+    isMaried: false
+};
+console.log(person.name);
+
+let arr = ['plum.png', 'orange.jpeg', 'apple.bmp'];
+console.log(arr[2]);
+// alert("Hello world!");
+// let answer = confirm("Are you here?");
+// console.log(answer);
+//всплывающее окно с вопросом, где "да" - ответ по умолч
+//"+" перед переменной превращает строку в число
+// let answer = +prompt("Есть ли вам 18?", "да");
+//проверка типа данных
+// console.log(typeof(answer));
+// console.log("arr" + "- object");
+let incr = 10,
+    decr = 10;
+
+    console.log(++incr);
+    console.log(decr--);
+    // проверка равенства по типу
+console.log("2" === 2);
